fdfec5b99b0a2134cc6051decb91624b64af1675
Gabriel Barn <gbrielbbarn@gmail.com>
2024-10-23 20:11:26 -0300

    [feat] myLibrary object array

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,17 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    title: "Prelude to Foundation",
+    author: "Isaac Asimov",
+    year: 1988,
+    bookCover: "Assets/book covers/Prelude_to_Foundation_cover.jpg",
+  },
+  {
+    title: "Taras Bulba",
+    author: "Nikolai Gogol",
+    year: 1835,
+    bookCover: "Assets/book covers/Taras_bulba_cover.jpg",
+  }
+];
 
 function Book() {
   // the constructor...
